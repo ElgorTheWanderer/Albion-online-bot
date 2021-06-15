7547405ea32e4324a88962a8acb1958b9bc34cad
Vadym Sokorenko <69482838+ElgorTheWanderer@users.noreply.github.com>
2021-06-15 22:23:19 +0300

    Issue #9: Initial commit.

---
+++ src/main/java/com/github/elgorthewanderer/albion_online_bot/AlbionOnlineBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.elgorthewanderer.albion_online_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbionOnlineBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbionOnlineBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/elgorthewanderer/albion_online_bot/AlbionOnlineBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.elgorthewanderer.albion_online_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbionOnlineBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

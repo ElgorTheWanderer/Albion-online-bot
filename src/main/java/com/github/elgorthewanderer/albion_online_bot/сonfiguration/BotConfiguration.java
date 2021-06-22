f9970aaa3dc736fb0a623a37d89cde1e0cfef87f
Stas Kolesnychenko <olimpixks@gmail.com>
2021-06-23 00:44:09 +0300

    Issue #10 Created Basic Bot & added bot token.

---
+++ src/main/java/com/github/elgorthewanderer/albion_online_bot/сonfiguration/BotConfiguration.java
@@ -0,0 +1,46 @@
+package com.github.elgorthewanderer.albion_online_bot.сonfiguration;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${DISCORD_BOT_TOKEN}")
+    private String token;
+
+    @Bean
+    public GatewayDiscordClient gatewayDiscordClient() {
+        GatewayDiscordClient client =
+                DiscordClientBuilder.create(token)
+                        .build()
+                        .login()
+                        .block();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    final User self = event.getSelf();
+                    System.out.println(String.format(
+                            "Logged in as %s#%s", self.getUsername(), self.getDiscriminator()
+                    ));
+                });
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
+
+        client.onDisconnect().block();
+        return client;
+    }
+}
